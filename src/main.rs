7d645e48f45f11fdd9a99d6ca38cb1d816947716
Aravindh Sridharan <mail@aravindh.io>
2018-11-30 08:50:18 +0100

    Parallelization WIP

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,40 @@ use std::fmt::Debug;
 use std::convert::From;
 use std::path::Display;
 use std::cmp::Ord;
+use std::sync::mpsc;
+use std::sync::mpsc::{Sender, Receiver};
+use std::thread;
 
 fn main() {
     let dir = std::env::args().nth(1);
     let mut files = analyzse_dir(&dir.unwrap()).unwrap();
-    files.sort_by(|a,b|b.size.cmp(&a.size));
+    files.sort_by(|a, b| b.size.cmp(&a.size));
     for file in files {
         println!("{}", file);
     }
 }
 
 fn analyzse_dir(dir: &str) -> io::Result<Vec<File>> {
+    let (tx, rx) = mpsc::channel();
+
     let mut data: Vec<File> = Vec::new();
     let files = fs::read_dir(dir)?;
     for file in files {
         let file = file?;
         let metadata = file.metadata()?;
-
         if metadata.is_dir() {
-            data.append(&mut analyzse_dir(&format!("{}", file.path().display()))?);
+            thread::spawn(|| {
+                let result1 = analyzse_dir(&format!("{}", &file.path().display())).unwrap();
+                tx.send(Ok(result1));
+            });
         } else {
-            let f = File { name: String::from(file.path().file_name().unwrap().to_str().unwrap()), size: metadata.len(), is_dir: metadata.is_dir() };
+            let name = String::from(file.path().file_name().unwrap().to_str().unwrap());
+            let f = File { name, size: metadata.len(), is_dir: metadata.is_dir() };
             data.push(f);
         }
     }
+    let result = rx.recv().unwrap();
+    data.append(&mut result.unwrap());
     return Ok(data);
 }
 
@@ -37,6 +47,10 @@ struct File {
     size: u64,
     is_dir: bool,
 }
+
+unsafe impl Send for File {}
+
+unsafe impl Sync for File {}
 
 impl std::fmt::Display for File {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
@@ -64,6 +78,7 @@ impl PartialEq for File {
         self.name == other.name
     }
 }
+
 impl Ord for File {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         self.size.cmp(&other.size)
